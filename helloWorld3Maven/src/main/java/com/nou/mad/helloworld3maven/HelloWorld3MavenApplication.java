c056f39d240e2ca1fc3a5b233af150e58215a3b7
mad <1naouguh@gmail.com>
2023-06-12 18:29:38 +0800

    spring boot 3 maven hello world

---
+++ helloWorld3Maven/src/main/java/com/nou/mad/helloworld3maven/HelloWorld3MavenApplication.java
@@ -0,0 +1,13 @@
+package com.nou.mad.helloworld3maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorld3MavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorld3MavenApplication.class, args);
+    }
+
+}
---
+++ helloWorld3Maven/src/test/java/com/nou/mad/helloworld3maven/HelloWorld3MavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nou.mad.helloworld3maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorld3MavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
