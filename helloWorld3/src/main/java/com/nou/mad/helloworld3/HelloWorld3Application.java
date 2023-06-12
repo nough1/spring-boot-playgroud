1f7e79c6ece519b0b8f70a3e64ce8b6008d996ea
mad <1naouguh@gmail.com>
2023-06-12 18:21:34 +0800

    first commit

---
+++ helloWorld3/src/main/java/com/nou/mad/helloworld3/HelloWorld3Application.java
@@ -0,0 +1,13 @@
+package com.nou.mad.helloworld3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorld3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorld3Application.class, args);
+    }
+
+}
---
+++ helloWorld3/src/test/java/com/nou/mad/helloworld3/HelloWorld3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nou.mad.helloworld3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorld3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
